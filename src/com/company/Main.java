f5451f7bdd1f1fceedace20552c74fad75ae5177
41m0 <steffenusalm@hotmail.com>
2017-06-05 15:28:38 +0200

    New Project Listview Part2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,11 +13,11 @@ import javafx.stage.Stage;
 
 //to use FX stuff, main must extend Application
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+public class Main extends Application {
 
     private Stage window;
-    private Button addTask, delTask;
-    private TextField t_addTask;
+    private Button delTask, delProject;
+    private TextField addTask, addProject;
     private ListView<String> tasks = new ListView<>();
     private ListView<String> projects = new ListView<>();
 
@@ -33,17 +33,14 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         window = primaryStage;
         window.setTitle("Working Title");
 
-        addTask = new Button("Add Task");
-        addTask.setOnAction((event) -> addTask());
+        /*
+        Tasklist
+         */
 
-
-
-        //Add task button, not really necessary?
-
-        t_addTask = new TextField();
-        // if(t_addTask.getText().isEmpty()) addTask.setDisable(true);
-        t_addTask.setPromptText("Define task");
-        t_addTask.setOnKeyPressed((event) -> {
+        addTask = new TextField();
+        // if(addTask.getText().isEmpty()) addTask.setDisable(true);
+        addTask.setPromptText("Define task");
+        addTask.setOnKeyPressed((event) -> {
             if(event.getCode() == KeyCode.ENTER) {
                 addTask(); }
         });
@@ -61,6 +58,32 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 
         delTask.setOnAction((ActionEvent e) -> delTask());
 
+        /*
+        Project list
+         */
+        addProject = new TextField();
+        // if(addTask.getText().isEmpty()) addTask.setDisable(true);
+        addProject.setPromptText("Define Project");
+        addProject.setOnKeyPressed((event) -> {
+            if(event.getCode() == KeyCode.ENTER) {
+                addProject(); }
+        });
+
+        //Multiselect
+        projects.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        projects.setPrefWidth(400);
+
+        projects.setOnKeyPressed((event) -> {
+            if(event.getCode() == KeyCode.DELETE){
+                delProject(); }
+        });
+
+        delProject = new Button("Delete Project");
+
+        delProject.setOnAction((ActionEvent e) -> delProject());
+
+
+
 
         //TitledPane tp = new TitledPane("My Titled Pane", new Button("Button"));
         Label projectLabel = new Label("Projects");
@@ -73,8 +96,8 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         VBox rightList = new VBox();
 
         topBar.getChildren().addAll(new Label("TopBar"));
-        leftList.getChildren().addAll(t_addTask,tasks, delTask);
-        rightList.getChildren().addAll(projects);
+        rightList.getChildren().addAll(addTask,tasks, delTask);
+        leftList.getChildren().addAll(addProject, projects, delProject);
         statusBar.getChildren().addAll(new Label("StatusBar"));
 
         borderPane.setTop(topBar);
@@ -87,8 +110,8 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         //Gridpane
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10,10,10,10));
-        grid.getColumnConstraints().add(new ColumnConstraints(300));
         grid.getColumnConstraints().add(new ColumnConstraints(500));
+        grid.getColumnConstraints().add(new ColumnConstraints(300));
         grid.setVgap(8);
         grid.setHgap(10);
 
@@ -109,18 +132,12 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         //Content inside the window: Scene
     }
 
-    @Override
-    public void handle(ActionEvent event){
-        if(event.getSource()==addTask){
-            addTask();
-        }
-    }
     private void addTask(){
-        String text = t_addTask.getText();
+        String text = addTask.getText();
         if(!tasks.getItems().contains(text)){
             NextStep nextStep = new NextStep(text);
             tasks.getItems().add(nextStep.toString());
-            t_addTask.clear();
+            addTask.clear();
         } else {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Notice");
@@ -129,12 +146,32 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 
             alert.showAndWait();
         }
+    }
 
+    private void addProject(){
+        String text = addProject.getText();
+        if(!projects.getItems().contains(text)){
+            NextStep nextStep = new NextStep(text);
+            projects.getItems().add(nextStep.toString());
+            addProject.clear();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Notice");
+            alert.setHeaderText(null);
+            alert.setContentText("Project already exists.");
+
+            alert.showAndWait();
+        }
     }
 
     private void delTask(){
         //Allows for multi-select delete
         ObservableList<String> list = tasks.getSelectionModel().getSelectedItems();
         tasks.getItems().removeAll(list);
+    }
+    private void delProject(){
+        //Allows for multi-select delete
+        ObservableList<String> list = projects.getSelectionModel().getSelectedItems();
+        projects.getItems().removeAll(list);
     }
 }
